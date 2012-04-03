b9aed328e2bd56dbddc3e28065ef06fd8118d410
Jane Peters <jpeters@iastate.edu>
2012-04-03 15:48:15 -0500

    created separate effect files

--- Scripts/Effects/lumos.lua
+++ Scripts/Effects/lumos.lua
@@ -7,38 +7,30 @@ require("getScriptFilename")
 vrjLua.appendToModelSearchPath(getScriptFilename())
 local device = gadget.PositionInterface("VJWand")
 
---Simple room models to demonstrate lighting capability.  
---TESTING AND DEBUGGING ONLY
-room = Transform{
-	position={0,0,0},
-	orientation=AngleAxis(Degrees(-90), Axis{0.0,0.0,0.0}),
-	scale=.1,
-	Model([[../../Hogwarts Models/OSG/Room of Requirement/room of requirements.ive]]),
-}
-RelativeTo.World:addChild(room)
-
 local stateSet = RelativeTo.World:getOrCreateStateSet()
 
 --Create first light & lightsource
 --Not required for lumos - serves as background lighting
-local light1 = osg.Light()
-local lightsource1 = osg.LightSource()
+light1 = osg.Light()
+--light1:setLightNum(0)
+lightsource1 = osg.LightSource()
 lightsource1:setLight(light1)
 
-local light2 = osg.Light()
+--Creat lumos/"flashlight" effect
+light2 = osg.Light()
 --Add if more than one light
 light2:setLightNum(1)
-local lightsource2 = osg.LightSource()
+lightsource2 = osg.LightSource()
 lightsource2:setLight(light2)
 
 --Regular lighting for when lumos is not in effect
-local light3 = osg.Light()
+light3 = osg.Light()
 light3:setLightNum(2)
-local lightsource3 = osg.LightSource()
+lightsource3 = osg.LightSource()
 lightsource3:setLight(light3)
-local light4 = osg.Light()
+light4 = osg.Light()
 light4:setLightNum(3)
-local lightsource4 = osg.LightSource()
+lightsource4 = osg.LightSource()
 lightsource4:setLight(light4)
 
 --Set ambient lighting - [R,G,B,A]
@@ -46,27 +38,22 @@ lightsource4:setLight(light4)
 --Set background lighting low
 light1:setAmbient(osg.Vec4(0.1, 0.1, 0.1, .5))
 --Set directed lighting to higher intensity
-light2:setAmbient(osg.Vec4(0.7, 0.8, .9, 1))
+light2:setAmbient(osg.Vec4(.6,.8,1,.5))
 --Set regular background to higher intensity
 light3:setAmbient(osg.Vec4(.8, .8, 0.8, 1))
-light4:setAmbient(osg.Vec4(0.8, 0.8, 0.8, 0.8))
+light4:setAmbient(osg.Vec4(.8, .8, 0.8, 1))
 
 --set diffuse lighting
 light1:setDiffuse(osg.Vec4(.1, .1, .1, .5))
-light2:setDiffuse(osg.Vec4(.1, .1, .3, .5))
-
+light2:setDiffuse(osg.Vec4(.3,.5,.8,.5))
 
 --Set attenuation (different amounts of light depending on distance)
 --Combine constant, linear and quadratic attenuation for desired effect
 light1:setConstantAttenuation(.001)
 light1:setLinearAttenuation(.05)
-
 light2:setConstantAttenuation(.001)
-light2:setLinearAttenuation(.00001)
---Adjust quadratic attenuation to impact depth of illumination
---NOTE: quadraticAttenuation give most realistic 
---	feel to the degree to which the light is attenuated
-light2:setQuadraticAttenuation(.005)
+light2:setLinearAttenuation(.0025)
+light2:setQuadraticAttenuation(.0005)
 
 --Set background light to always be present
 lightsource1:setLocalStateSetModes(osg.StateAttribute.Values.ON)
@@ -79,16 +66,16 @@ RelativeTo.World:addChild(lightsource1)
 RelativeTo.World:addChild(lightsource3)
 RelativeTo.World:addChild(lightsource4)
 
-light1:setPosition(osg.Vec4(1.5, 2, 0, 1.0))
 light3:setPosition(osg.Vec4(1.5, 2, 0, 1.0))
-light4:setPosition(osg.Vec4(0, 3, -5, 1.0))
+light4:setPosition(osg.Vec4(16,4-.56763+2,10, 1.0))
+light1:setPosition(osg.Vec4(2,3,8, 1.0))
 
 --Set width of beam of directed light
 --90 yeilds half sphere, 20 yeilds narrow beam
-light2:setSpotCutoff(20)
+light2:setSpotCutoff(12)
 --Set definition of beam edge
 --A higher SpotExponent softens the light at the outer edges of the beam
-light2:setSpotExponent(70)			
+light2:setSpotExponent(92)			
 RelativeTo.World:addChild(lightsource2)
 
 xform = osg.MatrixTransform()
@@ -105,14 +92,14 @@ updateFlashLightPos = function()
 	while true do
 		local newPos = xform:getMatrix():getTrans()
 		light2:setDirection(osg.Vec3(device.forwardVector:x(),device.forwardVector:y(),device.forwardVector:z()))
-		light2:setPosition(osg.Vec4(newPos:x(), newPos:y(), (newPos:z()+.25), 1.0))
+		light2:setPosition(osg.Vec4(newPos:x(), newPos:y(), (newPos:z()-.1), 1.0))
 		Actions.waitForRedraw()
 	end
 end
 --Actions.addFrameAction(updateFlashLightPos)
 
 lightONandOFF = function()
-	local drawBtn = gadget.DigitalInterface("VJButton1")
+	local drawBtn = gadget.DigitalInterface("VJButton2")
 	while true do
 		--keep drawing scene until button pressed
 		repeat
@@ -120,24 +107,24 @@ lightONandOFF = function()
 		until drawBtn.justPressed
 		
 		-- turn on the light
-		lightsource2:setLocalStateSetModes(osg.StateAttribute.Values.ON)
-		stateSet:setAssociatedModes(light2, osg.StateAttribute.Values.ON)
-		lightsource3:setLocalStateSetModes(osg.StateAttribute.Values.OFF)
-		stateSet:setAssociatedModes(light3, osg.StateAttribute.Values.OFF)
-		lightsource4:setLocalStateSetModes(osg.StateAttribute.Values.OFF)
-		stateSet:setAssociatedModes(light4, osg.StateAttribute.Values.OFF)		
+			lightsource2:setLocalStateSetModes(osg.StateAttribute.Values.ON)
+			stateSet:setAssociatedModes(light2, osg.StateAttribute.Values.ON)
+			lightsource3:setLocalStateSetModes(osg.StateAttribute.Values.OFF)
+			stateSet:setAssociatedModes(light3, osg.StateAttribute.Values.OFF)
+			lightsource4:setLocalStateSetModes(osg.StateAttribute.Values.OFF)
+			stateSet:setAssociatedModes(light4, osg.StateAttribute.Values.OFF)		
 		--keep drawing scene until button pressed
 		repeat
 			Actions.waitForRedraw()
 		until drawBtn.justPressed
 	
 		--turn off the light
-		lightsource2:setLocalStateSetModes(osg.StateAttribute.Values.OFF)
-		stateSet:setAssociatedModes(light2, osg.StateAttribute.Values.OFF)
-		lightsource3:setLocalStateSetModes(osg.StateAttribute.Values.ON)
-		stateSet:setAssociatedModes(light3, osg.StateAttribute.Values.ON)
-		lightsource4:setLocalStateSetModes(osg.StateAttribute.Values.ON)
-		stateSet:setAssociatedModes(light4, osg.StateAttribute.Values.ON)		
+			lightsource2:setLocalStateSetModes(osg.StateAttribute.Values.OFF)
+			stateSet:setAssociatedModes(light2, osg.StateAttribute.Values.OFF)
+			lightsource3:setLocalStateSetModes(osg.StateAttribute.Values.ON)
+			stateSet:setAssociatedModes(light3, osg.StateAttribute.Values.ON)
+			lightsource4:setLocalStateSetModes(osg.StateAttribute.Values.ON)
+			stateSet:setAssociatedModes(light4, osg.StateAttribute.Values.ON)		
 		end
 end
 
---
+++ Scripts/magicmain.lua
@@ -0,0 +1,24 @@
+require("Actions")
+require("TransparentGroup")
+require("getScriptFilename")
+vrjLua.appendToModelSearchPath(getScriptFilename())
+dofile(vrjLua.findInModelSearchPath([[Effects/lumos.lua]]))
+dofile(vrjLua.findInModelSearchPath([[Effects/draw.lua]]))
+local device = gadget.PositionInterface("VJWand")
+
+--Button Descriptions:
+--A:"name(METaL)=WMButtonA":"name(Computer)=VJButton0":flying effect
+--B:"name(METaL)=WMButtonB":"name(Computer)=VJButton1":drawing effect
+--Plus:"name(METaL)=WMButtonPlus":"name(Computer)=":lumos effect
+--Minus:"name(METaL)=WMButtonMinus":"name(Computer)="
+--Home:"name(METaL)=WMButtonHome":"name(Computer)=":clear drawing
+
+roomRequirement = Transform{
+	position={0,0,0},
+	orientation=AngleAxis(Degrees(-90), Axis{0.0,0.0,0.0}),
+	scale=.2,
+	Model([[../../Hogwarts Models/OSG/Room of Requirement/room of requirements expanded.ive]]),
+}
+RelativeTo.World:addChild(roomRequirement)
+
+
