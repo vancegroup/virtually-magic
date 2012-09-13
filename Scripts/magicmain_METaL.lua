1f27596f79c1676a9d2263d7c521a1bb0dea0cd7
unknown <atfleege@grantwood.vrac.iastate.edu>
2012-09-13 15:20:08 -0500

    Adding new models and movement for painting into maze.

--- Scripts/magicmain_METaL.lua
+++ Scripts/magicmain_METaL.lua
@@ -18,7 +18,7 @@ roomRequirement = Transform{
 	position={0,0,0},
 	orientation=AngleAxis(Degrees(-90), Axis{0.0,0.0,0.0}),
 	scale=.5,
-	Model([[../../Hogwarts Models/OSG/Room of Requirement/room of requirements expanded.ive]]),
+	Model([[../../Hogwarts Models/OSG/Room of Requirement/complete_room.ive]]),
 }
 RelativeTo.World:addChild(roomRequirement)
 
@@ -26,7 +26,7 @@ wardrobe = Transform{
 	position={0,0,0},
 	orientation=AngleAxis(Degrees(-90), Axis{0.0,0.0,0.0}),
 	scale=.5,
-	Model([[../../Hogwarts Models/OSG/Room of Requirement/wardrobe.ive]]),
+	Model([[../../Hogwarts Models/OSG/Room of Requirement/wardrobeback2.ive]]),
 }
 RelativeTo.World:addChild(wardrobe)
 
@@ -34,13 +34,27 @@ dementor = Transform{
 	position={0,0,0},
 	orientation=AngleAxis(Degrees(-90), Axis{0.0,0.0,0.0}),
 	scale=.5,
-	Model([[../../Hogwarts Models/OSG/Room of Requirement/ghost1.ive]]),
+	Model([[../../Hogwarts Models/OSG/Room of Requirement/boggart.ive]]),
 }
 
 boggart = TransparentGroup{
 	alpha = .7,
 	dementor
 }
+
+ladyPainting = Transform{
+	position={0,0,0},
+	orientation=AngleAxis(Degrees(-90), Axis{0.0,0.0,0.0}),
+	scale=.5,
+	Model([[../../Hogwarts Models/OSG/Room of Requirement/ladypainting.ive]]),
+}
+snitch = Transform{
+	position={1,0,0},
+	orientation=AngleAxis(Degrees(-90), Axis{0.0,0.0,0.0}),
+	scale=.5,
+	Model([[../../Hogwarts Models/OSG/Room of Requirement/snitch.ive]]),
+}
+RelativeTo.World:addChild(snitch)
 
 updatepositionTrack = function()
 	while true do
@@ -66,11 +80,11 @@ ghostAppear = function()
 end
 
 
-moveGhost = function()
+moveGhost = function(dt)
 	while true do
 			if (ghostPresent==true and (((dementor:getPosition()):z())<24)) then
-				dementor:setPosition(osg.Vec3d((dementor:getPosition()):x(), (dementor:getPosition()):y(), ((dementor:getPosition()):z()+.2)),1)
-				Actions.waitForRedraw()
+				dementor:setPosition(osg.Vec3d((dementor:getPosition()):x()+.95*dt, (dementor:getPosition()):y()-.04*dt, ((dementor:getPosition()):z()+ 4.2*dt--[[.25]])),1)
+				dt = Actions.waitForRedraw()
 			end
 		Actions.waitForRedraw()
 	end
@@ -78,3 +92,31 @@ end
 
 Actions.addFrameAction(updatepositionTrack)
 Actions.addFrameAction(ghostAppear)
+
+paintingPresent = false
+
+paintingAppear = function()
+	while true do
+			if (paintingPresent==false) then
+				RelativeTo.World:addChild(ladyPainting)
+				paintingPresent = true
+				break
+			end
+		Actions.waitForRedraw()
+	end
+	Actions.addFrameAction(moveladyPainting)
+end
+
+moveladyPainting = function(dt)
+	while true do
+			if ((track:x()> 2 and track:x()< 6 and track:z()> -23 and track:z()< -19 and (((ladyPainting:getPosition()):z())>-3))) then
+				ladyPainting:setPosition(osg.Vec3d((ladyPainting:getPosition()):x(), (ladyPainting:getPosition()):y(), ((ladyPainting:getPosition()):z()-.12*dt)))
+				dt = Actions.waitForRedraw()
+			end
+		Actions.waitForRedraw()
+	end
+end
+
+Actions.addFrameAction(paintingAppear)
+-- Actions.addFrameAction(printPos)
+
