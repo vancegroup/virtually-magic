f7e5140011b17d9f87ee737e1a59795b33bc0fab
unknown <atfleege@Eisenhower.vrac.iastate.edu>
2013-03-14 13:13:44 -0500

    changed animation to reflect new room geometry

--- Scripts/Effects/painting_move.lua
+++ Scripts/Effects/painting_move.lua
@@ -6,7 +6,7 @@ vrjLua.appendToModelSearchPath(fn)
 dofile(vrjLua.findInModelSearchPath([[MoveTools.lua]]))
 
 painting = Transform{
-	position={0,0,0},
+	position={0,-.4,0},
 	orientation=AngleAxis(Degrees(-90), Axis{0.0,0.0,0.0}),
 	scale=.5,
 	Model([[../../Hogwarts Models/OSG/Room of Requirement/ladypainting.ive]]),
@@ -14,13 +14,17 @@ painting = Transform{
 RelativeTo.World:addChild(painting)
 
 painting_move = function()
-	local Pos = function()
-		return {-.01,0,-3}
-	end
 	return function()
 		while true do
 				if ((track:x()> 2 and track:x()< 4 and track:z()> -21.5 and track:z()< -17 and (((painting:getPosition()):z())>-3))) then
-					newPos = osg.Vec3d(unpack(Pos()))
+					newPos = osg.Vec3d(-.01,-.4,-3)
+					newPos = newPos - painting:getPosition()
+					PlayPainting()
+					func = Transformation.move_slow(painting,.2,newPos:x(),newPos:y(),newPos:z())
+					func()
+					Actions.waitForRedraw()
+				elseif ((track:x()< 2 and track:z()< -21.5 and (((painting:getPosition()):z())<-2.9))) then
+					newPos = osg.Vec3d(0,-.4,0)
 					newPos = newPos - painting:getPosition()
 					PlayPainting()
 					func = Transformation.move_slow(painting,.2,newPos:x(),newPos:y(),newPos:z())
