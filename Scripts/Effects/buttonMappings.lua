80406ecc7e1cb9197eb6bb864e727b800761a68a
Leif Berg <lpberg@iastate.edu>
2015-03-07 13:53:57 -0600

    created single file for button mappings.

---
+++ Scripts/Effects/buttonMappings.lua
@@ -0,0 +1,11 @@
+ 
+--button mappings
+navSwitchBtn = gadget.DigitalInterface("WMButtonPlus")
+navRotate1 =  gadget.DigitalInterface("WMButtonRight")
+navRotate2 =  gadget.DigitalInterface("WMButtonLeft")
+lumosBtn = gadget.DigitalInterface("WMButtonUp")
+helpMenuBtn = gadget.DigitalInterface("WMButton2")
+drawClrBtn = gadget.DigitalInterface("VJButton0")
+drawBtn = gadget.DigitalInterface("VJButton2")
+drawChColorBtn = gadget.DigitalInterface("VJButton1")
+resetBtn = gadget.DigitalInterface("WMButtonHome")
